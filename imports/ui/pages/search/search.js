721b07b94550446b23e89dd3ee0623b92b6acefd
AMWJ <arielj@mit.edu>
2017-01-26 15:07:59 -0500

    Search page is dynamic.

--- imports/ui/pages/search/search.js
+++ imports/ui/pages/search/search.js
@@ -2,16 +2,33 @@ import { Books } from '/imports/api/books/books.js';
 import './search.html';
 import './search.css'
 
-$(document).on('click', '.panel-heading span.clickable', function(e){
-    var $this = $(this);
-    if(!$this.hasClass('panel-collapsed')) {
-		$this.parents('.panel').find('.panel-body').slideUp();
-		$this.addClass('panel-collapsed');
-		$this.find('i').removeClass('glyphicon-chevron-up').addClass('glyphicon-chevron-down');
-	} else {
-		$this.parents('.panel').find('.panel-body').slideDown();
-		$this.removeClass('panel-collapsed');
-		$this.find('i').removeClass('glyphicon-chevron-down').addClass('glyphicon-chevron-up');
-	}
-})
+Template.search.onCreated(function () {
+    Meteor.subscribe('books.public');
+});
 
+Template.search.helpers({
+    searchResults() {
+        return Books.find()
+    },
+    title() {
+        return this.title;
+    },
+    author() {
+        return this.author;
+    }
+});
+
+Template.search.events({
+    'click .panel-heading span.clickable'(event) {
+        var $this = $(event.target);
+        if (!$this.hasClass('panel-collapsed')) {
+            $this.parents('.panel').find('.panel-body').slideUp();
+            $this.addClass('panel-collapsed');
+            $this.find('i').removeClass('glyphicon-chevron-up').addClass('glyphicon-chevron-down');
+        } else {
+            $this.parents('.panel').find('.panel-body').slideDown();
+            $this.removeClass('panel-collapsed');
+            $this.find('i').removeClass('glyphicon-chevron-down').addClass('glyphicon-chevron-up');
+        }
+    }
+});
