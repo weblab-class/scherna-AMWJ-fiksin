75aec71ac52d83d7e6bcde0d0c38ae4bc2743b8f
Sammy Cherna <cherna@mit.edu>
2017-01-27 16:19:11 -0500

    new conversation button

--- imports/ui/pages/messages/messages.js
+++ imports/ui/pages/messages/messages.js
@@ -13,5 +13,14 @@ Template.messages.helpers({
 Template.messages.events({
     'click .conversation'(event) {
         FlowRouter.go("App.conversation", {_id: this._id});
+    },
+    'submit .newConversation'(event) {
+        event.preventDefault();
+        const username = event.target.username.value;
+        const conversation = new Conversation().save();
+        const userToAdd = Meteor.users.findOne({username});
+        conversation.addParticipant(userToAdd);
+        conversation.sendMessage("Hi " + username);
+        event.target.message.value = '';
     }
 });
