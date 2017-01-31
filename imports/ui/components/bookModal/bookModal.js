fba968feb42ee4d61dedb6fb97a46aae1b6e6985
fiksin <juliafiksinski@dhcp-18-189-62-67.dyn.MIT.EDU>
2017-01-31 02:14:00 -0500

    Added bookModal files and some updates to library

--- imports/ui/components/bookModal/bookModal.js
+++ imports/ui/components/bookModal/bookModal.js
@@ -1,6 +1,7 @@
 ﻿import { Books } from '/imports/api/books/books.js';
 import { Meteor } from 'meteor/meteor';
 import './bookModal.html';
+import './bookModal.css'
 
 Template.bookModal.onCreated(function () {
     console.log(this);
