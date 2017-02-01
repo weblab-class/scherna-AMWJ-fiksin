692cddfe5e3a305c3283bbf86b28f947fbc87f4f
AMWJ <arielj@mit.edu>
2017-02-01 17:27:02 -0500

    Fixed deleting Book Requests when the book is deleted.
    Max height of a message preview.

--- imports/api/books/methods.js
+++ imports/api/books/methods.js
@@ -2,7 +2,7 @@ import { Meteor } from 'meteor/meteor';
 import { check } from 'meteor/check';
 import { Books } from './books.js';
 import { Shelves } from '../shelves/shelves.js';
-import { BoobRequests } from '../bookRequests/bookRequests.js';
+import { BookRequests } from '../bookRequests/bookRequests.js';
 
 Meteor.methods({
     'books.create'(shelfId, title, author, isbn, pageCount, summary, bookUrl, imageUrl) {
